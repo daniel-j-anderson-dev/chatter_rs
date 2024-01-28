93a0468952f12ad3be83037e3714a7a8def8e457
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-01-27 23:20:53 -0500

    Created room

---
+++ src/network/room.rs
@@ -0,0 +1,10 @@
+use color_eyre::eyre::Result;
+use std::net::SocketAddr;
+
+pub fn host(host_ip: SocketAddr) -> Result<()> {
+    unimplemented!()
+}
+
+pub fn join(host_ip: SocketAddr) -> Result<()> {
+    unimplemented!()
+}
